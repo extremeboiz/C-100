c2ce8aa749935d0109935c37d8ea5bda23527078
Bihan <mailbihan2008@gmail.com>
2021-12-15 18:48:56 -0500

    Create car.py

---
+++ car.py
@@ -0,0 +1,21 @@
+class Car(object):
+    def __init__(self,model,color,company,speed_limit):
+        self.color="Orange"
+        self.company="Mclaren"
+        self.model="FireTail"
+        self.speed_limit=240
+
+def start(self):
+    print("Started")
+
+
+def stop(self):
+    print("Stopped")
+
+
+def accelarate(self):
+    print("Accelarating...")
+
+def change_gear(self, gear_type):
+    print("Gear Changed")
+        
